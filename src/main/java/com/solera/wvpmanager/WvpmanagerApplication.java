785cf6c1276e98a5f56ba8b920b44ff96b551a28
Padilla, Diego <diego.padilla2@solera.com>
2025-07-21 14:20:35 -0600

    Initial commit

---
+++ src/main/java/com/solera/wvpmanager/WvpmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.solera.wvpmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvpmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvpmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solera/wvpmanager/WvpmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solera.wvpmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WvpmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
